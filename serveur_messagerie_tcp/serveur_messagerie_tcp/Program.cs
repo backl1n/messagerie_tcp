36dcdaf84af8e9c03a4e34486c8d941d3f3a2a56
Simon Delmotte <simon.delmotte38200@gmail.com>
2019-11-24 19:22:50 +0100

    ajout de la solution serveur

---
+++ serveur_messagerie_tcp/serveur_messagerie_tcp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace serveur_messagerie_tcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
